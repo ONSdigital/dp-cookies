c8f7fce347118d60a927ff6268c68ef3c051172b
Jon Dewi Jones <jondewijones@gmail.com>
2020-02-12 08:37:21 +0000

    Add all cookies to set handler. Add new routes to test other cookie values

--- main.go
+++ main.go
@@ -13,13 +13,42 @@ var policy = cookies.Policy{
 }
 
 func main() {
+	var domain = "localhost"
 	http.HandleFunc("/set-cookies", func(w http.ResponseWriter, r *http.Request) {
-		cookies.SetPolicy(w, policy, "www.test.com")
-		cookies.SetPreferenceIsSet(w, "www.test.com")
+		fmt.Println("Setting all cookies")
+		cookies.SetPolicy(w, policy, domain)
+		cookies.SetPreferenceIsSet(w, domain)
+		cookies.SetLang(w, "en", domain)
+		cookies.SetCollection(w, "test-collection-id-123456789", domain)
+		cookies.SetUserAuthToken(w, "test-user-auth-token", domain)
 	})
 
 	http.HandleFunc("/cookies", func(w http.ResponseWriter, r *http.Request) {
 		cookiesResponse := cookies.GetCookiePreferences(r)
+		w.Write([]byte(fmt.Sprintf("%+v \n", cookiesResponse)))
+	})
+
+	http.HandleFunc("/lang", func(w http.ResponseWriter, r *http.Request) {
+		cookiesResponse, err := cookies.GetLang(r)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+		w.Write([]byte(fmt.Sprintf("%+v \n", cookiesResponse)))
+	})
+
+	http.HandleFunc("/collection", func(w http.ResponseWriter, r *http.Request) {
+		cookiesResponse, err := cookies.GetCollection(r)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+		w.Write([]byte(fmt.Sprintf("%+v \n", cookiesResponse)))
+	})
+
+	http.HandleFunc("/user-auth-token", func(w http.ResponseWriter, r *http.Request) {
+		cookiesResponse, err := cookies.GetUserAuthToken(r)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
 		w.Write([]byte(fmt.Sprintf("%+v \n", cookiesResponse)))
 	})
 
