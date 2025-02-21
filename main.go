de3818c073090983a89288ec2bdc5def6d4b79b1
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2025-02-21 15:03:42 +0000

    2553 add new cookies to local dev environment

--- main.go
+++ main.go
@@ -12,12 +12,21 @@ var policy = cookies.Policy{
 	Usage:     true,
 }
 
+var ONSPolicy = cookies.ONSPolicy{
+	Essential: true,
+	Settings:  true,
+	Usage:     true,
+	Campaigns: true,
+}
+
 func main() {
 	var domain = "localhost"
 	http.HandleFunc("/set-cookies", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Setting all cookies")
-		cookies.SetPolicy(w, policy, domain)  //nolint:staticcheck // To be removed in future iteration
+		cookies.SetPolicy(w, policy, domain) //nolint:staticcheck // To be removed in future iteration
+		cookies.SetONSPolicy(w, ONSPolicy, domain)
 		cookies.SetPreferenceIsSet(w, domain) //nolint:staticcheck // To be removed in future iteration
+		cookies.SetONSPreferenceIsSet(w, domain)
 		cookies.SetLang(w, "en", domain)
 		cookies.SetCollection(w, "test-collection-id-123456789", domain)
 		cookies.SetUserAuthToken(w, "test-user-auth-token", domain)
@@ -25,9 +34,17 @@ func main() {
 
 	http.HandleFunc("/cookies", func(w http.ResponseWriter, r *http.Request) {
 		cookiesResponse := cookies.GetCookiePreferences(r)
+		onsCookiesResponse := cookies.GetONSCookiePreferences(r)
+
 		_, err := fmt.Fprintf(w, "%+v \n", cookiesResponse)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		_, wErr := fmt.Fprintf(w, "%+v \n", onsCookiesResponse)
+		if wErr != nil {
+			http.Error(w, wErr.Error(), http.StatusInternalServerError)
 			return
 		}
 	})
